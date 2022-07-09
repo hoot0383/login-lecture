4ce53edff61fdcaf309aa0c33946a282671b9f1d
hoot0383 <hoot0383@naver.com>
2022-07-10 08:11:03 +0900

    프런트 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -22,9 +22,20 @@ const process = {
         const id = req.body.id,
             psword = req.body.psword;
         
-        console.log(id, psword);
-        
-    },
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx]=== psword){
+            return res.json({
+                success: true,
+            });
+        }
+    }
+
+    return res.json({
+        success:false,
+        msg: "로그인에 실패하셨습니다.",
+    });
+ },
 };
 
 
