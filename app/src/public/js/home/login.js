1e1038c8f1f32391c2d982f1199def87ce918106
hoot0383 <hoot0383@naver.com>
2022-07-11 19:01:45 +0900

    로그인 화면 꾸미기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const id = document.querySelector("#id"),
-    password = document.querySelector("#psword"),
+    psword = document.querySelector("#psword"),
     loginBtn = document.querySelector("button");
 
 
